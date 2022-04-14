8666b5e87b1932a35cc8617e219a28320c5f4a70
sumitra05 <sumitra9039sumi@gmail.com>
2022-04-15 01:18:55 +0530

    changes in projects-card

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,7 +6,7 @@ export const projectsData = [
 			"This is a clone of Kickstarter which maintains a global crowdfunding platform focused on creativity.",
 		tags: ["materialui", "Redux", "React", "Styled-components"],
 		logos: ["fas fa-atom", "fab fa-react", "fab fa-node"],
-		code: "https://github.com/HewaliGogoi/kickstarter",
+		code: "https://github.com/sumitra05/Kickstarter-Clone",
 		demo: "https://apna-kickstarter.netlify.app",
 		image: "./Kickstarter.png",
 	},
@@ -21,37 +21,5 @@ export const projectsData = [
 		demo: "https://english-hindi-translator.netlify.app",
 		image: "./Translator.png",
 	},
-	// {
-	// 	id: 3,
-	// 	projectName: "Vacation",
-	// 	projectDesc:
-	// 		"Vacation-Travel Website is a web application that allows users to book Travel packages to enjoy sightseeing in a new destination.",
-	// 	tags: ["Html", "css", "javascript"],
-	// 	logos: ["fab fa-html5", "fab fa-css3", "fab fa-js"],
-	// 	code: "https://github.com/sivasangari03-G/TravelNow",
-	// 	demo: "https://vacation-travel.herokuapp.com/",
-	// 	image: "https://user-images.githubusercontent.com/77038735/157721466-bf633b57-acb4-4522-8c0f-d7c678dab3a9.PNG",
-	// },
-	// {
-	// 	id: 4,
-	// 	projectName: "Blackjack Game",
-	// 	projectDesc:
-	// 		"Blackjack is a classic casino game of luck and strategy.The player can randomly pick multiple number of cards and the card values are summed up. If sum is 21 then 'Blackjack'.",
-	// 	tags: ["Html", "css", "javascript"],
-	// 	logos: ["fab fa-html5", "fab fa-css3", "fab fa-js"],
-	// 	code: "https://github.com/sivasangari03-G/blackJack",
-	// 	demo: "https://gameblackjack.herokuapp.com/",
-	// 	image: "https://user-images.githubusercontent.com/77038735/157721587-da8bf677-e73d-418f-bdb1-388577b7b557.PNG",
-	// },
-	// {
-	// 	id: 5,
-	// 	projectName: "Mini Nike Store",
-	// 	projectDesc:
-	// 		"Shoe Store is an e-commerce website for buying shoes and user can sort all the shoes by price and select accordingly. ",
-	// 	tags: ["Html", "css", "javascript"],
-	// 	logos: ["fab fa-html5", "fab fa-css3", "fab fa-js"],
-	// 	code: "https://github.com/sivasangari03-G/first-ecommerce/tree/master/practice",
-	// 	demo: "https://mini-nike-store.herokuapp.com/",
-	// 	image: "https://user-images.githubusercontent.com/77038735/157721638-777459be-6772-40e7-b485-fd144a853f9b.PNG",
-	// },
+	
 ];
